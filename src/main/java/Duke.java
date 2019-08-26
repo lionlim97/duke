fb158acc6c5f6b55e0e795c628ad1c67f88bf16a
Lionel <lionlim97@hotmail.com>
2019-08-26 15:19:12 +0800

    Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 
 public class Duke {
 
-    private static String input;
-    private static ArrayList<String> list;
+    private static String input, command, line;
+    private static int number;
+    private static ArrayList<Task> list;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -22,29 +23,51 @@ public class Duke {
         System.out.println();
 
         while (true) {
-            input = sc.nextLine();
-            if (input.equals("bye")) {
+            command = sc.next();
+            if (command.equals("bye")) {
                 System.out.println();
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println();
                 break;
             }
-            else if(input.equals("list")){
+            else if(command.equals("list")){
                 System.out.println();
+                System.out.println("Here are the tasks in your list:");
                 for(int i = 1; i <= list.size(); i++){
-                    System.out.println(i + ". " + list.get(i - 1));
+                    System.out.println(i + ".[" + list.get(i-1).getStatusIcon()
+                            + "] " + list.get(i - 1).getDescription());
                 }
                 System.out.println();
             }
+            else if(command.equals("done")){
+                number = sc.nextInt();
+                if(number > list.size()){
+                    System.out.println("Error, number entered is more than size of list");
+                    System.out.println();
+                    continue;
+                }
+                System.out.println("Nice! I've marked this task as done:");
+                list.get(number-1).setDone(true);
+                System.out.println("[" + list.get(number-1).getStatusIcon() + "] "
+                        + list.get(number-1).getDescription());
+                System.out.println();
+            }
             else{
+                input = sc.nextLine();
+                if(input.isEmpty()){
+                    line = command;
+                }
+                else{
+                    line = command + input;
+                }
                 Echo();
-                list.add(input);
+                list.add(new Task(line));
             }
         }
     }
     private static void Echo(){
         System.out.println();
-        System.out.println("added: " + input);
+        System.out.println("added: " + line);
         System.out.println();
     }
 }
